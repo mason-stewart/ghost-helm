b61e8b48bc2d630bd0a6d177fedd878b1cbbe3c1
Mason Stewart <masondesu@gmail.com>
2014-08-03 13:24:32 -0400

    Initial commit. Roughed out super basic functionality and got a few tests passing.

---
+++ README.md
@@ -0,0 +1,44 @@
+# ghost-helm [![Build Status](https://secure.travis-ci.org/masondesu/ghost-helm.png?branch=master)](http://travis-ci.org/masondesu/ghost-helm)
+
+> A set of build tools for The Iron Yard
+
+
+## Getting Started
+
+Install the module with: `npm install ghost-helm`
+
+```js
+var ghost-helm = require('ghost-helm');
+ghost-helm.awesome(); // "awesome"
+```
+
+Install with cli command
+
+```sh
+$ npm install -g ghost-helm
+$ ghost-helm --help
+$ ghost-helm --version
+```
+
+
+
+
+## Documentation
+
+_(Coming soon)_
+
+
+## Examples
+
+_(Coming soon)_
+
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com).
+
+
+## License
+
+Copyright (c) 2014 Mason Stewart  
+Licensed under the MIT license.
---
+++ test/main.js
@@ -0,0 +1,135 @@
+/*global describe, it*/
+"use strict";
+
+require("mocha");
+
+var config = {
+  distDir: 'test/.tmp-dist',
+
+  templatesDir: 'test/fixtures/templates/pages/**/*.jade',
+  templatesBaseDir: 'test/fixtures/templates',
+  templatesDistDir: 'test/.tmp',
+
+  stylesDir: 'test/fixtures//styles/main.scss',
+  stylesDistDir: 'test/.tmp/styles',
+
+  jsDir: 'test/fixtures/scripts/**/*.js',
+
+  assetBuildDir: 'test/.tmp/**/*.html',
+  userefSearchPath: ['test/fixtures/','test/.tmp'],
+
+  imagesDir: ['test/fixtures/images/**/*', 'test/fixtures/bower_components/ghost-shield/dist/images/**/*'],
+  imagesDistDir: 'test/.tmp-dist/images',
+
+  fontDir: ['test/fixtures/bower_components/ghost-shield/dist/webfonts/*'],
+
+  cname: 'test/fixtures/CNAME',
+
+  sitemapDir: 'test/.tmp-dist/**/*.html',
+  siteUrl: 'http://theironyard.com',
+
+  cleanDir: ['test/.tmp', 'test/.tmp-dist'],
+
+  // deployCommand: 'sh deploy.sh',
+  deployCommand: 'ls',
+}
+
+var expect = require("chai").expect,
+    gulp = require("gulp"),
+    gutil = require("gulp-util"),
+    ghostHelm = require("../"),
+    fs = require('fs'),
+    Q = require('q'),
+    generateDeferred = Q.defer(),
+
+    expectedAboutContent,
+    expectedIndexContent,
+    expectedFaqContent,
+    aboutContent,
+    indexContent,
+    faqContent;
+
+ghostHelm.setup(config, gulp);
+console.log('Ghost Helm is made possible by contributions from viewers like you.');
+
+expectedAboutContent = fs.readFileSync('test/expected/about/index.html').toString();
+expectedIndexContent = fs.readFileSync('test/expected/index.html').toString();
+expectedFaqContent = fs.readFileSync('test/expected/academy/faq/index.html').toString();
+
+gulp.task('generate', ['deploy'], function(){
+  
+ 
+  generateDeferred.resolve();
+
+
+});
+gulp.start('generate');
+
+
+before(function(done){
+  this.timeout(60000)
+
+  generateDeferred.promise.then(function () {
+    aboutContent = fs.readFileSync('test/.tmp-dist/about/index.html').toString();
+    indexContent = fs.readFileSync('test/.tmp-dist/index.html').toString();
+    faqContent = fs.readFileSync('test/.tmp-dist/academy/faq/index.html').toString();
+    done();
+  })
+
+})
+
+
+describe("Ghost Helm", function(){
+
+  describe("should produce files that", function() {
+    it("exactly match the expected content of the About page", function(){
+      expect(aboutContent).to.equal(expectedAboutContent);
+    });
+
+    it("exactly match the expected content of the Index page", function(){
+      expect(indexContent).to.equal(expectedIndexContent);
+    });
+
+    it("exactly match the expected content of the FAQ page", function(){
+      expect(faqContent).to.equal(expectedFaqContent);
+    }); 
+  });
+});
+
+
+
+
+
+
+// Example config... should be turned into defaults.
+
+// var config = {
+//   distDir: 'dist',
+
+//   templatesDir: 'test/fixtures/templates/pages/**/*.jade',
+//   templatesBaseDir: 'test/fixtures/templates',
+//   templatesDistDir: 'test/.tmp',
+
+//   stylesDir: 'app/styles/main.scss',
+//   stylesDistDir: '.tmp/styles',
+
+//   jsDir: 'app/scripts/**/*.js',
+
+//   assetBuildDir: '.tmp/**/*.html',
+//   userefSearchPath: ['app','.tmp'],
+
+//   imagesDir: ['app/images/**/*', 'app/bower_components/ghost-shield/dist/images/**/*'],
+//   imagesDistDir: 'dist/images',
+
+//   fontDir: ['app/bower_components/ghost-shield/dist/webfonts/*'],
+
+//   cname: 'CNAME',
+
+//   sitemapDir: 'dist/**/*.html',
+//   siteUrl: 'http://theironyard.com',
+
+//   cleanDir: ['.tmp', 'dist'],
+
+//   // deployCommand: 'sh deploy.sh',
+//   deployCommand: 'ls',
+// }
