36becf978c5dec8286acbb6bc578acc25f572039
Mason Stewart <masondesu@gmail.com>
2014-08-12 11:37:50 -0400

    Simplified the test fixtures

--- test/fixtures/scripts/main.js
+++ test/fixtures/scripts/main.js
@@ -3,48 +3,6 @@
 
 'use strict';
 
-function wpFeed(url, $el) {
-   $.ajax({
-     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-     dataType: 'json',
-     success: function(data) {
-       
-       var entries = data.responseData.feed.entries;
-       var fragment = '';
-       
-       if(entries) {
-          
-          for(var i=0;i < 4; i++) {
-            if (entries[i]) {
-              fragment += '<li><a href="'+entries[i].link+'">'+entries[i].title+'</a></li>';
-            }
-          }
-         
-         $el.append(fragment);
-         }
-       }
-   });
-}
-
-function storiesFeed(url, $el) {
-   $.ajax({
-     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-     dataType: 'json',
-     success: function(data) {
-       
-       var entries = data.responseData.feed.entries;
-       var fragment = '';
-       
-       if(entries) {
-          
-          for(var i=0;i < 3; i++) {
-            if (entries[i]) {
-              fragment += '<li><a href="'+entries[i].link+'">'+entries[i].title+'</a></li>';
-            }
-          }
-         
-         $el.append(fragment);
-         }
-       }
-   });
+function cool (){
+  return true;
 }
